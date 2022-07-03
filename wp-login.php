49adde9dcdb088c2b20d2824944822de254503d4
has_san1122 <faisalraj596@gmail.com>
2022-07-03 20:28:54 +0100

    demo import

--- wp-login.php
+++ wp-login.php
@@ -2,14 +2,14 @@
 <html lang="en-US">
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>Log In &lsaquo; qq &#8212; WordPress</title>
+	<title>Log In &lsaquo; Restaurant Blog &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
 <link rel="dns-prefetch" href="//s.w.org">
-<link rel="stylesheet" id="dashicons-css" href="/wp-includes/css/dashicons.min.css?ver=6.0" media="all">
-<link rel="stylesheet" id="buttons-css" href="/wp-includes/css/buttons.min.css?ver=6.0" media="all">
-<link rel="stylesheet" id="forms-css" href="/wp-admin/css/forms.min.css?ver=6.0" media="all">
-<link rel="stylesheet" id="l10n-css" href="/wp-admin/css/l10n.min.css?ver=6.0" media="all">
-<link rel="stylesheet" id="login-css" href="/wp-admin/css/login.min.css?ver=6.0" media="all">
+<link rel="stylesheet" id="dashicons-css" href="/wp-includes/css/dashicons.min.css?ver=6.0" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="/wp-includes/css/buttons.min.css?ver=6.0" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="/wp-admin/css/forms.min.css?ver=6.0" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="/wp-admin/css/l10n.min.css?ver=6.0" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="/wp-admin/css/login.min.css?ver=6.0" type="text/css" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
 		</head>
@@ -51,34 +51,42 @@
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 				<p id="backtoblog">
-			<a href="/">&larr; Go to qq</a>		</p>
+			<a href="/">&larr; Go to Restaurant Blog</a>		</p>
 			</div>
-			<script src="/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
-<script src="/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
-<script id="zxcvbn-async-js-extra">
+			<script type="text/javascript" src="/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
+<script type="text/javascript" src="/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+<script type="text/javascript" id="zxcvbn-async-js-extra">
+/*  */
 var _zxcvbnSettings = {"src":"httplocalhost:8012\/wordpress-6.0\/wordpress\/wp-includes\/js\/zxcvbn.min.js"};
+/*  */
 </script>
-<script src="/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
-<script src="/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.9" id="regenerator-runtime-js"></script>
-<script src="/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
-<script src="/wp-includes/js/dist/hooks.min.js?ver=c6d64f2cb8f5c6bb49caca37f8828ce3" id="wp-hooks-js"></script>
-<script src="/wp-includes/js/dist/i18n.min.js?ver=ebee46757c6a411e38fd079a7ac71d94" id="wp-i18n-js"></script>
-<script id="wp-i18n-js-after">
+<script type="text/javascript" src="/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script type="text/javascript" src="/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.9" id="regenerator-runtime-js"></script>
+<script type="text/javascript" src="/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script type="text/javascript" src="/wp-includes/js/dist/hooks.min.js?ver=c6d64f2cb8f5c6bb49caca37f8828ce3" id="wp-hooks-js"></script>
+<script type="text/javascript" src="/wp-includes/js/dist/i18n.min.js?ver=ebee46757c6a411e38fd079a7ac71d94" id="wp-i18n-js"></script>
+<script type="text/javascript" id="wp-i18n-js-after">
 wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
-<script id="password-strength-meter-js-extra">
+<script type="text/javascript" id="password-strength-meter-js-extra">
+/*  */
 var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
+/*  */
 </script>
-<script src="/wp-admin/js/password-strength-meter.min.js?ver=6.0" id="password-strength-meter-js"></script>
-<script src="/wp-includes/js/underscore.min.js?ver=1.13.3" id="underscore-js"></script>
-<script id="wp-util-js-extra">
+<script type="text/javascript" src="/wp-admin/js/password-strength-meter.min.js?ver=6.0" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="/wp-includes/js/underscore.min.js?ver=1.13.3" id="underscore-js"></script>
+<script type="text/javascript" id="wp-util-js-extra">
+/*  */
 var _wpUtilSettings = {"ajax":{"url":"\/wordpress-6.0\/wordpress\/wp-admin\/admin-ajax.php"}};
+/*  */
 </script>
-<script src="/wp-includes/js/wp-util.min.js?ver=6.0" id="wp-util-js"></script>
-<script id="user-profile-js-extra">
+<script type="text/javascript" src="/wp-includes/js/wp-util.min.js?ver=6.0" id="wp-util-js"></script>
+<script type="text/javascript" id="user-profile-js-extra">
+/*  */
 var userProfileL10n = {"user_id":"0","nonce":"eae6ee96b8"};
+/*  */
 </script>
-<script src="/wp-admin/js/user-profile.min.js?ver=6.0" id="user-profile-js"></script>
+<script type="text/javascript" src="/wp-admin/js/user-profile.min.js?ver=6.0" id="user-profile-js"></script>
 	<div class="clear"></div>
 	</body>
 	</html>
